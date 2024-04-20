7c1a8c20443812a78a269aa36bbdfaf88d50e6b7
Mohan <miq5018@gmail.com>
2024-04-20 15:14:40 -0700

    Made some minor changes

--- ai-generator/src/main/java/com/example/aigenerator/controller/AIController.java
+++ ai-generator/src/main/java/com/example/aigenerator/controller/AIController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.aigenerator.model.DietPreferences;
@@ -19,32 +18,16 @@ public class AIController {
     
     @Autowired
     AIService aiService;
-
-   
+    
     /**
      * Retrieves an AI-generated response based on provided parameters for meal planning.
      * 
-     * @param dietType               The type of diet preferred by the user.
-     * @param cuisine                The preferred cuisine for meal planning.
-     * @param calories               The target daily calorie intake for the planned meals.
-     * @param mealTimeString         The frequency of the main meals (e.g., breakfast, lunch, dinner) in one day.
-     * @param snackTimeString        The frequency of snacks in one day.
-     * @param allergySelectedItems   Items the user is allergic to or intolerence with and wants to avoid in meal planning.
-     * @param dislikeSelectedItems   Items the user dislikes and wants to avoid in meal planning.
-     * @return                       The AI-generated response in String for meal planning based on the provided parameters.
+     * @param userPreferences The user's preferences for generating the meal plan.
+     * @return AI-generated response in String
      */
-    /*
-    @GetMapping("/generate")
-    public String getResponse(@RequestParam String dietType, @RequestParam String cuisine, @RequestParam int calories, @RequestParam String mealTimeString, @RequestParam String snackTimeString, @RequestParam String allergySelectedItems, @RequestParam String dislikeSelectedItems){
-        return aiService.response(dietType, cuisine, calories, mealTimeString, snackTimeString, allergySelectedItems, dislikeSelectedItems);
-    }
-    */
-    
     @GetMapping("/generate")
     public String getResponse(@ModelAttribute DietPreferences userPreferences){
         return aiService.response(userPreferences);
     }
-    
-
 }
  
--- ai-generator/src/main/java/com/example/aigenerator/services/AIService.java
+++ ai-generator/src/main/java/com/example/aigenerator/services/AIService.java
@@ -25,47 +25,9 @@ public class AIService {
     /**
      * Gets the response from GPT for meal planning based on provided parameters and prompt template.
      * 
-     * @param dietType             The type of diet preferred by the user.
-     * @param cuisine              The preferred cuisine for meal planning.
-     * @param calories             The target daily calorie intake for the planned meals.
-     * @param mealTimeString       The frequency of main meals (e.g., breakfast, lunch, dinner) in one day.
-     * @param snackTimeString      The frequency of snacks in one day.
-     * @param allergySelectedItems Items the user is allergic to or intolerent with and wants to avoid in meal planning.
-     * @param dislikeSelectedItems Items the user dislikes and wants to avoid in meal planning.
-     * @return                     An AI-generated response String for meal planning in the provided JSON format.
+     * @param userPreferences The user's preferences for generating the meal plan.
+     * @return AI-generated response in String
      */
-    /*
-    public String response(String dietType, String cuisine, int calories, String mealTimeString, String snackTimeString, String allergySelectedItems, String dislikeSelectedItems){
-        //Get the desired JSON response format from the template file
-        String mealPlanTemplate = "";
-        try {
-            //Read the content of the JSON file into a byte array
-            byte[] encodedBytes = Files.readAllBytes(Paths.get("src/main/resources/meal_plan_template.json"));
-            //Convert the byte array to a string using UTF-8 encoding
-            mealPlanTemplate = new String(encodedBytes, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        //Formulate the prompt message using provided parameters and JSON template
-        String promptMessage = "Please generate a one-day healthy and balanced meal plan with portion sizes and meeting the following requirements:\n" + 
-                                "Diet Type: " + dietType + "\n" +
-                                "Cuisine: " + cuisine + "\n" +
-                                "Calorie Needs: " + calories + " kcals/day\n" + 
-                                "Food Allergies/Intolerance: " + allergySelectedItems + ", please do not include these or related foods\n" +
-                                "Disliked food: " + dislikeSelectedItems + ", please do not include these or related foods" + 
-                                "Please respond following this JSON format:" + mealPlanTemplate + "\n" + 
-                                "This JSON format template contains 6 meals and 6 snacks. Please adjust the template to generate the meal plan with" + 
-                                mealTimeString + " meals and " + snackTimeString + " snacks while still meeting other requirements listed above.";
-        
-        System.out.println(promptMessage);
-
-        Prompt prompt = new Prompt(promptMessage);
-        return this.chatClient.call(prompt).toString();
-    }
-    */
-
-    
     public String response(DietPreferences userPreferences){
         //Get the desired JSON response format from the template file
         String mealPlanTemplate = "";
@@ -86,7 +48,6 @@ public class AIService {
         String allergySelectedItems = userPreferences.getAllergySelectedItems();
         String dislikeSelectedItems = userPreferences.getDislikeSelectedItems();
 
-
         //Formulate the prompt message using provided parameters and JSON template
         String promptMessage = "Please generate a one-day healthy and balanced meal plan with portion sizes and meeting the following requirements:\n" + 
                                 "Diet Type: " + dietType + "\n" +
@@ -102,6 +63,5 @@ public class AIService {
 
         Prompt prompt = new Prompt(promptMessage);
         return this.chatClient.call(prompt).toString();
-    }
-    
+    } 
 }
